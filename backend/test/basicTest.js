c4ff86f240164fac971218a5e81c700e00fe324d
prashant-dhiru <prashant.dhiru@gmail.com>
2020-08-04 14:48:33 +0530

    2nd client test refined

--- backend/test/basicTest.js
+++ backend/test/basicTest.js
@@ -11,32 +11,16 @@ const socketOption = {
 describe('Suite of unit tests', function() {
 
     let client1;
-
-    beforeEach(function(done){
-        client2 = io.connect('http://localhost:3000', socketOption);
-
-        client2.on('connect', function() {
-            done();
-        });
-    });
-
-    afterEach(function(done) {
-        // Cleanup
-        
-        if(client2.connected) {
-            client2.disconnect();
-        }
-        done();
-    });
  
-    describe('First (hopefully useful) test', function() {
+    describe('Server-Client test', function() {
+        
         it('client1 connect to server', function(done) {
             client1 = io.connect('http://localhost:3000', socketOption);
             client1.on('connect', function(data){
                 client1.on('hello', function(data) {
                     try{
                         client1.disconnect();
-                        expect(data).to.equal('word');
+                        expect(data).to.equal('world');
                         done();
                     }
                     catch(e){
@@ -47,9 +31,10 @@ describe('Suite of unit tests', function() {
         });
 
         it('client2 connect to server', function(done) {
-            client2.on('hello', function(data) {
-                expect(data).to.equal('world');
-                done();
+            client2 = io.connect('http://localhost:3000', socketOption);
+            client2.on('connect', function() {            
+                    client2.disconnect();
+                    done();
             });
         });
     });
